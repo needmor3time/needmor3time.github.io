1732dea4941c865c8832d34813dc6abb464f81e9
Needmor3time <tbfarms72@gmail.com>
2018-08-24 21:43:18 -0600

    Norse game and gify

--- assignment_five/assets/javascript/game.js
+++ assignment_five/assets/javascript/game.js
@@ -2,120 +2,187 @@
 $(document).ready(function () {
 
     var game = {
-        norseGods =["question1", "question2", "question3", "question4", "question5", "question6", "question7", "question8", "question9", "question10", "question11", "question12"],
-
-        questions = {
-            question1: ["How did Odin lose his eye", "In the battle with Jodenheim", "Pulled out by an eagle while on a quest", "To win all battles until the end of time", "To drink from the pool of the Norns"],
-            question2: ["What are the names of Loki's offspring", "Hella and Fenrir", "Frigg and Frigga", "Hella and Freyja", "Hoognir and Munjir"],
-            question3: ["Which god or godess lost their hand to a giant wolf", "Balder", "Tyr", "Heimdal", "Rigg"],
-            question4: ["What is one of Heimdal's disguised names", "Munin", "Sigfruin", "Rigg", "Bouldin"],
-            question5: ["Which one of the following is NOT one of the nine realms", "Hafleheim", "Nifleheim", "Helheim", "Vaniheim"],
-            question6: ["How many children did Thor have", "None", "1", "2", "3"],
-            question7: ["Who gave birth to Odin's horse, Sleipnir", "the horse that pulls the world around the sun", "the frost giant's King's horse", "Loki", "Fenris"],
-            question8: ["Who wept tears of gold when they thought they were going to have to marry and ugly stone smith", "Frigga", "Freyja", "Indunna", "Sif"],
-            question9: ["What food kept the gods youthful", "Mead", "Honey", "Nectar", "Apples"],
-            question10: ["What creature does Thor kill, but in turn dies from his wounds in Ragnarok", "Jormongand", "Fenrir", "Loki", "Surter"],
-            question11: ["What is the name of Odin's spear", "Mjolnir", "Gungnir", "Yggridsil", "Hagrath"],
-            question12: ["Who kills the god Kvasir", "Jormongand", "Fenrir", "dwarves", "dark elves"]
-        },
-
-        correctAnswers = {
-            answer1 : questions.question1[4],
-            answer2 : questions.question2[1],
-            answer3 : questions.question3[2],
-            answer4 : questions.question4[3],
-            answer5 : questions.question5[1],
-            answer6 : questions.question6[4],
-            answer7 : questions.question7[3],
-            answer8 : questions.question8[2],
-            answer9 : questions.question9[4],
-            answer10 : questions.question10[1],
-            answer11 : questions.question11[2],
-            answer12 : questions.question12[3],
-        },
-
-        correct = 0,
-        wrong = 0,
-
-
-
-        startGame: function () {
-
-            //  Click events are done for us:
-            $("#stop").click(stopwatch.stop);
-            $("#reset").click(stopwatch.reset);
-            $("#start").click(stopwatch.start);
-        },
-
+        questions : [
+            {
+                question : "How did Odin lose his eye",
+                answers : ["In the battle with Jodenheim", "Pulled out by an eagle while on a quest", "To win all battles until the end of time", "To drink from the pool of the Norns"],
+                answer : "To drink from the pool of the Norns",
+            },
+            {
+                question : "What are the names of Loki's offspring",
+                answers : ["Hella and Fenrir", "Frigg and Frigga", "Hella and Freyja", "Hoognir and Munjir"],
+                answer : "Hella and Fenrir",
+            },
+            {
+                question : "Which god or godess lost their hand to a giant wolf",
+                answers : ["Balder", "Tyr", "Heimdal", "Rigg"],
+                answer : "Tyr",
+            },
+            {
+                question : "What is one of Heimdal's disguised names",
+                answers : ["Munin", "Sigfruin", "Rigg", "Bouldin"],
+                answer : "Rigg",
+            },
+            {
+                question : "Which one of the following is NOT one of the nine realms",
+                answers : ["Hafleheim", "Nifleheim", "Helheim", "Vaniheim"],
+                answer : "Hafleheim",
+            },
+            {
+                question : "How many children did Thor have",
+                answers : ["None", "1", "2", "3"],
+                answer : "3",
+            },
+            {
+                question : "Who gave birth to Odin's horse, Sleipnir",
+                answers : ["the horse that pulls the world around the sun", "the frost giant's King's horse", "Loki", "Fenris"],
+                answer : "3",
+            },
+            {
+                question : "Who wept tears of gold when they thought they were going to have to marry and ugly stone smith",
+                answers : ["Frigga", "Freyja", "Indunna", "Sif"],
+                answer : "Freyja",
+            },
+            {
+                question : "What food kept the gods youthful",
+                answers : ["Mead", "Honey", "Nectar", "Apples"],
+                answer : "Apples",
+            },
+            {
+                question : "What creature does Thor kill, but in turn dies from his wounds in Ragnarok",
+                answers : ["Jormongand", "Fenrir", "Loki", "Surter"],
+                answer : "Apples",
+            },
+            {
+                question : "What is the name of Odin's spear",
+                answers : ["Mjolnir", "Gungnir", "Yggridsil", "Hagrath"],
+                answer : "Gungnir",
+            },
+            {
+                question : "Who kills the god Kvasir",
+                answers : ["Jormongand", "Fenrir", "dwarves", "dark elves"],
+                answer : "dwarves",
+            },
+        ],
+        correct : 0,
+        wrong : 0,
+        time: 60,
         //  Variable that will hold our setInterval that runs the stopwatch
-        intervalId,
+        intervalId : 0,
 
-        // prevents the clock from being sped up unnecessarily
-        clockRunning = false,
+
 
         //  Our stopwatch object.
-        stopwatch = {
+        stopwatch : {
 
-            time: 0,
-
+            
             reset: function () {
 
-                stopwatch.time = 0;
-                $("#display").html("00");
+                
+                $("#display").html("00");//display doesn't not exist in html
             },
 
             start: function () {
 
-                //  TODO: Use setInterval to start the count here and set the clock to running.
-                if (!clockRunning) {
-                    clockRunning = true;
-                    intervalId = setInterval(stopwatch.count, 60000); //1 minute interval
-                };
-
+                //  Set time, Use setInterval to start the count here and set the clock to running.
+                time = 60;
+                clearInterval(game.intervalId);
+                game.intervalId = setInterval(this.count, 1000);
             },
             stop: function () {
 
                 //  TODO: Use clearInterval to stop the count here and set the clock to not be running.
-                clearInterval(intervalId);
-                clockRunning = false;
+                clearInterval(game.intervalId);
+                console.log("interval after clear", game.intervalId);
+                submitGame();
             },
 
             count: function () {
-                stopwatch.time--;
-                if (stopwatch.time != 0) {
-                    //if time doesn't equal 0, continue
+                //display time each time count fires (should be ever 1000ms)
+                $('.timer').text(this.time);
+                //if time doesn't equal 0, decrement time and check again for 0
+                if (this.time != 0) {
+                    this.time--;
+                    if (this.time == 0) {
+                        //stop time, tally totals, alert totals, end game
+                        console.log("display time", this.time);
+                        stop();
+                    };
+                    
 
                 }
-                else if (stopwatch.time == 0) {
-                    //stop counter, tally totals, alert totals, end game
-                    $("#display").html(stopwatch.time);
-                    console.log(stopwatch.time);
-                };
+                // else if (this.time == 0) {
+                //     //stop time, tally totals, alert totals, end game
+                //     console.log("this won't run", this.time);
+                //     stop();
+                // };
             },
             //function to update multiple choice
             multipleGuess: function () {
                 $('.question-box').html("");
-                for (var j = 1; j < 12; j++) {
-                    $('.question-box').append($("<h4>" + questions[j].question + "</h4>"));
-                    for (var i = 1; i < questions[j].question[i].length; i++) {
-                        $('.question-box').append($("<input type='radio' value='" + questions[j].answers[i] + "' name='question-" + j + "'>" + questions[j].answers[i] + "<br>"));
+                for (var j = 0; j < game.questions.length; j++) {
+                    console.log("game questions", game.questions[j]);
+                    $('.question-box').append($("<h4>" + game.questions[j].question + "</h4>"));
+                    for (var i = 0; i < game.questions[j].answers.length; i++) {
+                        console.log("answers for each question", game.questions[j].answers[i]); 
+                        $('.question-box').append($("<input type='radio' value=' " + game.questions[j].answers[i] + "' name='question-" + j + "'>" + game.questions[j].answers[i] + "<br>"));
                     }
                     $('.question-box').append('<hr>');
                 };
             },
 
+            submitGame : function () {
+                for (var j = 0; j < game.questions.length; j++)  {
+                    console.log("position in array", j);
+                    $.each($("input[name='question-" + j + "']:checked"), function() {
+                        var userGuess = $(this).attr('value');
+                        console.log("userGuess", userGuess);
+                        console.log("correctAnswer", game.questions[j].answer);
+                        if (userGuess === game.questions[j].answer) {
+                            console.log('correct');
+                            correct++;
+                        } else {
+                            console.log('incorrect');
+                            incorrect++;
+                        }
+                    });  
+                }
+                $('.correct').text(correct);
+                $('.incorrect').text(incorrect);
+                $('.end-screen').show();
+            },
+
+            newGame : function () {
+                correct = 0;
+                incorrect = 0;
+                // shuffleQuestions();
+                // shuffleAnswers();
+                this.multipleGuess();
+                this.start();
+                $(".end-screen").hide();
+            },
+
+            
+
+           
 
 
 
 
-        },
-    
-        $("#start").click(function () {
-            game.startGame();
-        }),
+        }
     };
-});
+    game.stopwatch.newGame();
 
+    $("#submitGame").on('click', function() {
+        game.stopwatch.submitGame();
+        stop();
+    });
+
+    $('#newGame').on('click', function() {
+        game.stopwatch.newGame();
+    });
+        
+});
 
 
 
---
+++ assignment_five/assets/javascript/temp.js
@@ -0,0 +1,64 @@
+var game = {
+    questions : [
+        {
+            question : "How did Odin lose his eye",
+            answers : ["In the battle with Jodenheim", "Pulled out by an eagle while on a quest", "To win all battles until the end of time", "To drink from the pool of the Norns"],
+            answer : "To drink from the pool of the Norns",
+        },
+        {
+            question : "What are the names of Loki's offspring",
+            answers : ["Hella and Fenrir", "Frigg and Frigga", "Hella and Freyja", "Hoognir and Munjir"],
+            answer : "Hella and Fenrir",
+        },
+        {
+            question : "Which god or godess lost their hand to a giant wolf",
+            answers : ["Balder", "Tyr", "Heimdal", "Rigg"],
+            answer : "Tyr",
+        },
+        {
+            question : "What is one of Heimdal's disguised names",
+            answers : ["Munin", "Sigfruin", "Rigg", "Bouldin"],
+            answer : "Rigg",
+        },
+        {
+            question : "Which one of the following is NOT one of the nine realms",
+            answers : ["Hafleheim", "Nifleheim", "Helheim", "Vaniheim"],
+            answer : "Hafleheim",
+        },
+        {
+            question : "How many children did Thor have",
+            answers : ["None", "1", "2", "3"],
+            answer : "3",
+        },
+        {
+            question : "Who gave birth to Odin's horse, Sleipnir",
+            answers : ["the horse that pulls the world around the sun", "the frost giant's King's horse", "Loki", "Fenris"],
+            answer : "3",
+        },
+        {
+            question : "Who wept tears of gold when they thought they were going to have to marry and ugly stone smith",
+            answers : ["Frigga", "Freyja", "Indunna", "Sif"],
+            answer : "Freyja",
+        },
+        {
+            question : "What food kept the gods youthful",
+            answers : ["Mead", "Honey", "Nectar", "Apples"],
+            answer : "Apples",
+        },
+        {
+            question : "What creature does Thor kill, but in turn dies from his wounds in Ragnarok",
+            answers : ["Jormongand", "Fenrir", "Loki", "Surter"],
+            answer : "Apples",
+        },
+        {
+            question : "What is the name of Odin's spear",
+            answers : ["Mjolnir", "Gungnir", "Yggridsil", "Hagrath"],
+            answer : "Gungnir",
+        },
+        {
+            question : "Who kills the god Kvasir",
+            answers : ["Jormongand", "Fenrir", "dwarves", "dark elves"],
+            answer : "dwarves",
+        },
+    ]
+}
