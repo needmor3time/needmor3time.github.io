dc53c52a4d3cb0c0ed9d3f4932a47cb96224464c
Doug <tbfarms72@gmail.com>
2018-10-09 20:39:12 -0600

    adding star wars files for heroku deploy

--- assignment_three/assets/javascript/game.js
+++ assignment_three/assets/javascript/game.js
@@ -5,12 +5,12 @@ var greekGods = ["zuess", "hades", "posiden", "hera", "aphrodite", "hermes", "at
 var hints = {
 zuess : ["male", "oldest brother", "most powerful"],
 hades : ["male", "big three", "underworld"],
-posiden : ["male", "big three", "water lover"],
+poseiden : ["male", "big three", "water lover"],
 hera : ["female", "amazon enforcer", "wife of the big guy"],
 aphrodite : ["female", "fairest of the gods", "love not war"],
 hermes : ["male", "messenger", "florist symbol with winged feet"],
 athena : ["female", "warrior goddess", "patron of Athens"],
-aries : ["male", "Zuess’s son", "war god"],
+ares : ["male", "Zuess’s son", "war god"],
 apollo : ["male", "Zuess’s son", "sun god"],
 kronus : ["male", "titan", "killed by his sons"]
 }
@@ -26,20 +26,32 @@ var hangManImages = ["./assets/images/Hangman-0.png", "./assets/images/Hangman-1
 
 function resetGame () {
     numOfGuesses = 6;
-    guessedLetters = [];
+    //empties guessed letters array and empties screen
+    for (var h = guessedLetters.length; h > 0; h--) {
+        guessedLetters.pop();
+        console.log("letters in guessedLetters array left", guessedLetters[g]);
+    };
+    //empties guessed letters array and empties screen
+    $("#display_guesses").html("<p></p>");
     for (var g = dashes.length; g > 0; g--) {
         dashes.pop();
+        console.log("letters in dashes array left", dashes[g]);
     };
+    $("#display_dashes").html("<p></p>");
+    //reset hangman pic and messages to empty
     $("#hangmanPic").attr("src","./assets/images/Hangman-0.png");
+    $("#messages").html("<p id = 'messagess'></p>");
     newGame();
 };
 
 function anotherRound () {
-    alert("Do you want to play another round?");
-    $(".messages").html("Do you want to play another game?");
+    console.log("Do you want to play another round?");
+    $("#messages").html("<p id = 'messagess'>Do you want to play another round?</p>");
+    setTimeout(resetGame, 3000);
 };
 
 function newGame () {
+    $("#messages").html("<p id = 'messagess'>Press any key to begin guessing.</p>");
     // Randomly chooses a choice from the options array. This is the Computer's guess.
     var computerChoice = greekGods[Math.floor(Math.random() * greekGods.length)];
     console.log(computerChoice);
@@ -48,6 +60,7 @@ function newGame () {
     //load dashes array with actual dashes
     for (var i = 0; i < computerChoice.length; i++) {
         dashes.push(" _ ");
+        $("#display_dashes").append(" _ ");
     }
 
         /* button_press = start game loop */
@@ -58,7 +71,7 @@ function newGame () {
             //condition of picking the same letter more than once
             if (guessedLetters.indexOf(key_guess) > -1) {
                     //do nothing if already guessed letter
-                    alert("You already guessed that letter.");
+                    $("#messages").html("<p id = 'messagess'>You already guessed that letter.</p>");
                     console.log('already guessed');
                 
             }
@@ -79,28 +92,25 @@ function newGame () {
                 // update guessedLetters array
                 guessedLetters.push(event.key);
                 console.log("correct guessedLetters", guessedLetters);
-                $(".display_guesses").html(dashes);
+                $("#display_dashes").html(dashes);
+                $("#display_guesses").append(event.key);
                 //checks dashes array for dashes to see if they have guessed all letters correctly
                 if (dashes.indexOf(" _ ") == -1)  {
                     console.log('game won');
+                    $('#myModal').modal('show');
                     //if there are no dashes, increment wins
                     wins++;
                     $("#wins").html("<a id = 'wins'>Wins = "+ wins +"</a>");
                     //if wins = 6, end the game
                     if (wins == 6) {
-                        alert("You know your Greek gods!");
+                        $("#messages").html("<p id = 'messagess'>You know your Greek gods!</p>");
                         // end game
                         return;
                     }
                     //if wins don't equal 6, send message and continue
                     else {
-                        //add some sort of wait or condition of updating the dashes prior to alert of win
-                        
-                        alert("You won this round!");
-                        setTimeout(resetGame, 2000)
+                        $("#messages").html("<p id = 'messagess'>You won this round!</p>");
                         setTimeout(anotherRound, 3000);
-
-
                     }
                 }
                 
@@ -111,6 +121,7 @@ function newGame () {
                 //update guessedLetters array
                 guessedLetters.push(event.key);
                 console.log("incorrect guessedLetters", guessedLetters);
+                $("#display_guesses").append(event.key);
                 //update numOfGuesses -1  embed if statement to check for numOfGuesses = 0
                 numOfGuesses--;
                 //update hangman progression linked to the number of guesses remaining and change image
@@ -128,36 +139,29 @@ function newGame () {
                     losses++;
                     $("#hangmanPic").attr("src","./assets/images/Hangman-6.png");
                     $(".num_guesses_left").html("<a>You have 0 guesses left</a>");
-                    alert("You lost this round!");
-                    setTimeout(resetGame, 1000)
+                    $("#messages").html("<p id = 'messagess'>You lost this round!</p>");
                     setTimeout(anotherRound, 3000);
                 }
                 else if (numOfGuesses == 4) {
                     $("#hangmanPic").attr("src","./assets/images/Hangman-2.png");
                     $(".num_guesses_left").html("<a>You have 4 guesses left</a>");
                     console.log(hints[computerChoice][0]);
-                    alert(hints[computerChoice][0]);
+                    $("#messages").html("<p id = 'messagess'>Hint: " + hints[computerChoice][0] + "</p");
                     
                 }
                 else if (numOfGuesses == 2) {
                     $("#hangmanPic").attr("src","./assets/images/Hangman-4.png");
                     $(".num_guesses_left").html("<a>You have 2 guesses left</a>");
                     console.log(hints[computerChoice][1]);
-                    alert(hints[computerChoice][1]);
+                    $("#messages").html("<p id = 'messagess'>Hint: " + hints[computerChoice][1] + "</p");
                 }
                 else if (numOfGuesses == 1) {
                     $("#hangmanPic").attr("src","./assets/images/Hangman-5.png");
                     $(".num_guesses_left").html("<a>You have 1 guess left</a>");
                     console.log(hints[computerChoice][2]);
-                    alert(hints[computerChoice][2]);
+                    $("#messages").html("<p id = 'messagess'>Hint: " + hints[computerChoice][2] + "</p");
                 }
             }
-
-
-
-
-
-
         }
 };
 newGame();
