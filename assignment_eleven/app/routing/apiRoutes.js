bb454a123f2e5c22735bace81f4fdbcc80dbb5ad
Doug <tbfarms72@gmail.com>
2018-10-23 21:51:27 -0600

    heroku deploy

--- assignment_eleven/app/routing/apiRoutes.js
+++ assignment_eleven/app/routing/apiRoutes.js
@@ -8,3 +8,11 @@ module.exports = function(app) {
 
     });
   }
+
+  // take inputs from user
+
+  // loop through object and compare. get absolute value of compare
+
+  //write results of each compare to an array.
+
+  //compare all the results, one with the lowest value is winner
