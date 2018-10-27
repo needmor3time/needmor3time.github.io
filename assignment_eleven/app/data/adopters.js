30c1ed96c20367d64066a83e95878cd3a2ab1b7c
Doug <tbfarms72@gmail.com>
2018-10-27 14:09:38 -0600

    redeploying heroku

---
+++ assignment_eleven/app/data/adopters.js
@@ -0,0 +1,49 @@
+var Adopters = [
+    {"name":"Doug",
+    "photo":"http://bigdogshugepaws.com/adoption/available-dogs/aristotle-neapolitan-mastiff-new-to-rescue-under-evaluation",
+    "scores":[
+        1,
+        2,
+        2,
+        3,
+        2,
+        2,
+        2,
+        1,
+        3,
+        1
+    ]},
+
+    {"name":"Carroll",
+    "photo":"http://bigdogshugepaws.com/adoption/available-dogs/justice-english-mastiff-new-to-rescue-under-evaluation",
+    "scores":[
+        1,
+        2,
+        3,
+        3,
+        3,
+        1,
+        4,
+        2,
+        3,
+        2
+    ]},
+
+    {"name":"Kasey",
+    "photo":"http://bigdogshugepaws.com/adoption/available-dogs/black-cherry-great-dane-mix",
+    "scores":[
+        1,
+        1,
+        1,
+        1,
+        1,
+        1,
+        2,
+        1,
+        2,
+        1
+    ]}
+  
+]
+
+module.exports = Adopters;
--- assignment_eleven/app/routing/apiRoutes.js
+++ assignment_eleven/app/routing/apiRoutes.js
@@ -1,12 +1,20 @@
 var furryFriends = require("../data/friends.js");
+var adopters = require("../data/adopters.js"); // <--add file for people adopting
 module.exports = function(app) {
     app.get("/api/friends", function(req, res) {
         res.json(furryFriends);
     });
+    app.get("/api/adopters", function(req, res) {
+        res.json(adopters);
+    });
+
+
+    var abDifference = 0;
 
     app.post("/api/friends", function(req, res) {
-        var newAdopter = req.body
-        //console.log("request: ", req);
+        var newAdopter = req.body;
+        var adopterScore = newAdopter.scores;
+        console.log("adopterScore: ", adopterScore);
         var bestDog = {
             name: "",
             photo: "",
@@ -16,9 +24,15 @@ module.exports = function(app) {
             var currentDog = furryFriends[i];
             for(var j=0; j < currentDog.scores.length; j++) {
                 var dogScore = currentDog.scores[j];
-                dogScore Math.abs // <-- absolute value
-            }
-        }
+                abDifference += Math.abs(parseInt(adopterScore) - parseInt(dogScore));
+                console.log("absolute difference", abDifference);
+                if (abDifference <= bestDog.bestScore) {
+                    bestDog.name = currentDog.name;
+                    bestDog.photo = currentDog.photo;
+                    bestDog.bestScore = abDifference;
+                };
+            };
+        };
         //
         console.log("new adopter info console", newAdopter);
     });
