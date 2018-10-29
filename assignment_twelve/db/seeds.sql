98eb8c38051edba0051b102017d8eb3250d0b938
Doug <tbfarms72@gmail.com>
2018-10-28 20:17:52 -0600

    initial commit of burgers app

---
+++ assignment_twelve/db/seeds.sql
@@ -0,0 +1,5 @@
+INSERT INTO burgers (name) VALUES ('Plain');
+INSERT INTO burgers (name) VALUES ('Extreme');
+INSERT INTO burgers (name, devoured) VALUES ('Everything', true);
+INSERT INTO burgers (name, devoured) VALUES ('Bleu', true);
+INSERT INTO burgers (name, devoured) VALUES ('Shroom', true);
