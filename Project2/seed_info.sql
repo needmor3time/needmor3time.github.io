04749a194c703272a36bb3557aa859650e05abf9
Doug <tbfarms72@gmail.com>
2018-11-17 09:46:28 -0700

    portfolio text and added greek app

--- Project2/seed_info.sql
+++ Project2/seed_info.sql
@@ -2,4 +2,4 @@ INSERT INTO dogs (name, photo, other_dogs, cats, exercise, special, age, childer
 
 INSERT INTO dogs (name, photo, other_dogs, cats, exercise, special, age, childern, size, fur, food, protective, gender, experience) VALUES ('Justice', 'photo_goes_here', 1, 2, 3, 3, 3, 1, 4, 2, 3, 2, 1, 0);
 
-INSERT INTO dogs (name, photo, other_dogs, cats, exercise, special, age, childern, size, fur, food, protective, gender, experience) VALUES ('Black_Cherry', 'photo_goes_here', 1, 1, 1, 1, 1, 1, 2, 1, 2, 1, 1, 0);
+INSERT INTO dogs (name, photo, other_dogs, cats, exercise, special, age, childern, size, fur, food, protective, gender, experience) VALUES ('Black Cherry', 'photo_goes_here', 1, 1, 1, 1, 1, 1, 2, 1, 2, 1, 1, 0);
--- assignment_eleven/app/routing/htmlRoutes.js
+++ assignment_eleven/app/routing/htmlRoutes.js
@@ -6,6 +6,6 @@ var path = require("path");
     });
 
     app.get("*", function(req, res) {
-        res.sendFile(path.join(__dirname, "../public/home.html"));
+        res.sendFile(path.join(__dirname, "../public/survey.html"));
     });
   }
